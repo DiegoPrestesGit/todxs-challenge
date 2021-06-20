6e9f14524599872ce3ba6fd88f3b4d9e26c49dad
Diego Prestes <diegoprestes.git@hotmail.com>
2021-06-20 19:30:11 -0300

    feat: add utils layer with error handling and http standards

---
+++ src/utils/errors.ts
@@ -0,0 +1,21 @@
+export class ServerError extends Error {
+  constructor(stack: string | undefined) {
+    super('Internal server error')
+    this.name = 'ServerError'
+    this.stack = stack
+  }
+}
+
+export class MissingParamError extends Error {
+  constructor(paramName: string) {
+    super(`Missing param ${paramName}`)
+    this.name = 'MissingParamError'
+  }
+}
+
+export class InvalidParamError extends Error {
+  constructor(paramName: string) {
+    super(`Invalid param: ${paramName}`)
+    this.name = 'InvalidParamError'
+  }
+}
